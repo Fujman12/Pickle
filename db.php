6784cd3c00a4164335f9fda45bc4094c0c0f9149
Armen Martikian <armenbizz@gmail.com>
2021-04-28 12:00:37 +0300

    db insert

--- db.php
+++ db.php
@@ -5,11 +5,30 @@ $password = "3asZuzlh";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
-echo $_POST['userName'];
 
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+echo $_POST['userName'];
+
+$userName = $_POST['userName'];
+$userPhone = $_POST['userPhone'];
+$userEmail = $_POST['userEmail'];
+
+$query = spintf("INSERT INTO Leads (name, phone, email)
+		VALUES ('%s', '%s', '%s')",
+ 		mysql_real_escape_string($userName),
+    mysql_real_escape_string($userPhone),
+  	mysql_real_escape_string($userEmail));
+
+if ($conn->query($query) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $query . "<br>" . $conn->error;
+}
+
+$conn->close();
+
 ?>
