321b40b099c2e43707a5ff87cc3cc87b8d6dcf3a
Armen Martikian <armenbizz@gmail.com>
2021-04-28 12:05:37 +0300

    dbname

--- db.php
+++ db.php
@@ -2,9 +2,10 @@
 $servername = "ku421687.mysql.ukraine.com.ua";
 $username = "ku421687_db";
 $password = "3asZuzlh";
+$dbname = "ku421687_db"
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
